e0d3ede8bc97c1a7f0d74f3f998d3e735ea2cc09
newmanuevers <seangnewman@msn.com>
2018-11-12 18:18:49 -0700

    Add project files.

---
+++ Udemy_Generics_dotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Generics_dotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
